e1c3acbde4080985030bd75f920176f40cc5da6a
Joris van de Donk <jorisvddonk@gmail.com>
2017-05-07 13:07:45 +0200

    Livereload via webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 var path = require('path');
+var LiveReloadPlugin = require('webpack-livereload-plugin');
 
 module.exports = {
   entry: './client/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve('./client/dist')
-  }
+  },
+  plugins: [
+    new LiveReloadPlugin({appendScriptTag: true})
+  ]
 };
