ae2d9b7b4f6894e127f32ed9d01375cf28aea7ad
Joris van de Donk <jorisvddonk@gmail.com>
2017-05-07 14:53:57 +0200

    Added working parallax

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
-import keycodes from './lib/keycodes'
+import keycodes from './lib/keycodes';
+import ParallaxLayer from './lib/Parallaxlayer';
 const PIXI = require('pixi.js');
 const koaws = require('koa-ws/client');
 
@@ -11,17 +12,15 @@ var keyState = keycodes(window, ["ArrowLeft", "ArrowUp", "ArrowDown", "ArrowRigh
 var app = new PIXI.Application(800, 600, {backgroundColor : 0x000000});
 rootElement.appendChild(app.view);
 
-var addParallaxLayer = function(assetLocation) {
-  var layer = new PIXI.Sprite(PIXI.Texture.fromImage(assetLocation));
-  layer.position.x = 0;
-  layer.position.y = 0;
-  app.stage.addChild(layer);
+var addParallaxLayer = function(assetLocation, depth) {
+  var layer = new ParallaxLayer({assetLocation: assetLocation, depth: depth})
+  layer.wire(app);
 };
 
-addParallaxLayer("assets/parallax_1_0.png");
-addParallaxLayer("assets/parallax_2.png");
-addParallaxLayer("assets/parallax_3.png");
-addParallaxLayer("assets/parallax_4.png");
+addParallaxLayer("assets/parallax_1_0.png", 4);
+addParallaxLayer("assets/parallax_2.png", 3);
+addParallaxLayer("assets/parallax_3.png", 2);
+addParallaxLayer("assets/parallax_4.png", 1);
 
 var ship = new PIXI.Sprite(PIXI.Texture.fromImage("assets/ship.png"));
 ship.position.x = 400;
@@ -36,12 +35,6 @@ ship.state = {
 app.stage.addChild(ship);
 
 app.ticker.add(function(delta) {
-  // center ship in stage
-  app.stage.pivot.x = ship.position.x;
-  app.stage.pivot.y = ship.position.y;
-  app.stage.position.x = app.renderer.width*0.5;
-  app.stage.position.y = app.renderer.height*0.5;
-
   // Handle keypresses for ship
   if (keyState.ArrowLeft) {
     ship.rotation -= 0.1
@@ -57,4 +50,10 @@ app.ticker.add(function(delta) {
   // Update ship position
   ship.position.x += ship.state.velocity.x;
   ship.position.y += ship.state.velocity.y;
+
+  // center ship in stage
+  app.stage.pivot.x = ship.position.x;
+  app.stage.pivot.y = ship.position.y;
+  app.stage.position.x = app.renderer.width*0.5;
+  app.stage.position.y = app.renderer.height*0.5;
 });
---
+++ client/lib/ParallaxLayer.js
@@ -0,0 +1,49 @@
+import _ from 'lodash';
+const PIXI = require('pixi.js');
+
+export default function(options) {
+  var container = new PIXI.Container();
+  var texture = PIXI.Texture.fromImage(options.assetLocation);
+  var rendererWidth;
+  var rendererHeight;
+  var depthMultiplier = 1 / options.depth || 1;
+
+  var update = function() {
+    if (texture.width !== 1 && rendererWidth && rendererHeight) {
+      // create textures
+      var numHorizontal = Math.max(2, parseInt(rendererWidth / texture.width)+2);
+      var numVertical = Math.max(2, parseInt(rendererHeight / texture.height)+2);
+      _.each(_.range(0, numHorizontal), function(x){
+        _.each(_.range(0, numVertical), function(y){
+          var sprite = new PIXI.Sprite(texture);
+          sprite.parallax = {
+            x: x,
+            y: y
+          }
+          sprite.position.x = x * texture.width;
+          sprite.position.y = y * texture.height;
+          container.addChild(sprite);
+        });
+      });
+    }
+  }
+
+  texture.on('update', update);
+  
+  container.wire = function(app) {
+    app.stage.addChild(container);
+    rendererWidth = app.renderer.width;
+    rendererHeight = app.renderer.height;
+    app.ticker.add(function(delta) {
+      // sync to center of screen
+      container.position.x = app.stage.pivot.x;
+      container.position.y = app.stage.pivot.y;
+      
+      // set offset
+      container.pivot.x = (app.stage.pivot.x*depthMultiplier % texture.width) + (container.width*0.5 - texture.width*0.5);
+      container.pivot.y = (app.stage.pivot.y*depthMultiplier % texture.height) + (container.height*0.5 - texture.height*0.5);
+    });
+    update();
+  }
+  return container;
+};
