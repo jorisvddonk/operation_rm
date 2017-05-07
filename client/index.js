9904d1eff732969e43cdbf2d134dea683c6f4470
Joris van de Donk <jorisvddonk@gmail.com>
2017-05-07 13:08:25 +0200

    added koa-ws

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 const PIXI = require('pixi.js');
+const koaws = require('koa-ws/client');
 
 var app = new PIXI.Application(800, 600, {backgroundColor : 0xffffff});
 document.getElementById('main').appendChild(app.view);
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const koaStatic = require('koa-static');
 const Koa = require('koa');
+const koaws = require('koa-ws');
 const app = new Koa();
 
+app.use(koaws(app, {
+  serveClientFile: true,
+  clientFilePath: '/koaws.js',
+  heartbeat: true,
+  heartbeatInterval: 1000
+}));
 app.use(koaStatic('client'));
 
 app.listen(8099);
