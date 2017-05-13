79471602c70510754f2187980ba492540fc21df6
Joris van de Donk <jorisvddonk@gmail.com>
2017-05-13 19:03:26 +0200

    Refactored File into an ES6 class extending PIXI.Container

--- client/lib/File.js
+++ client/lib/File.js
@@ -1,45 +1,50 @@
 import _ from 'lodash';
 const PIXI = require('pixi.js');
-
-export default function(options) {
-  var container = new PIXI.Container();
-  var fileSprite = new PIXI.Sprite(PIXI.Texture.fromImage("assets/file.png"));
-  fileSprite.anchor.set(0.5);
-  var damageSprites = new PIXI.Container();
-  _.each(_.range(1, 10), function(i) {
-    var sprite = new PIXI.Sprite(PIXI.Texture.fromImage("assets/dmg_" + i + ".png"));
-    sprite.visible = false;
-    sprite.anchor.set(0.5);
-    damageSprites.addChild(sprite);
-  });
-  var text = new PIXI.Text(options.filename,{fontFamily : 'Arial', fontSize: 10, fill : 'white', align : 'center', stroke: 'black', strokeThickness: 2});
-  text.anchor.set(0.5);
-  text.position.y = 40;
-  container.addChild(fileSprite);
-  container.addChild(text);
-  container.addChild(damageSprites);
-  
-  container.wire = function(app) {
-    // nothing needed here so far!
-  }
-  container.state = {
-    hitpoints: 10
-  }
-  container.gfxTick = function() {
-    _.each(damageSprites.children, function(sprite, i){
+export default class File extends PIXI.Container {
+  constructor(options) {
+    super();
+    var fileSprite = new PIXI.Sprite(PIXI.Texture.fromImage("assets/file.png"));
+    fileSprite.anchor.set(0.5);
+    this.damageSprites = new PIXI.Container();
+    _.each(_.range(1, 10), (i) => {
+      var sprite = new PIXI.Sprite(PIXI.Texture.fromImage("assets/dmg_" + i + ".png"));
       sprite.visible = false;
-      if (9 - container.state.hitpoints === i) {
+      sprite.anchor.set(0.5);
+      this.damageSprites.addChild(sprite);
+    });
+
+    var text = new PIXI.Text(options.filename,{fontFamily : 'Arial', fontSize: 10, fill : 'white', align : 'center', stroke: 'black', strokeThickness: 2});
+    text.anchor.set(0.5);
+    text.position.y = 40;
+    this.addChild(fileSprite);
+    this.addChild(text);
+    this.addChild(this.damageSprites);
+
+    this.state = {
+      hitpoints: 10
+    }
+  }
+
+  wire() {
+      // nothing needed here so far!
+  }
+  
+  gfxTick() {
+    _.each(this.damageSprites.children, (sprite, i) => {
+      sprite.visible = false;
+      if (9 - this.state.hitpoints === i) {
         sprite.visible = true;
       }
     })
   }
-  container.lifetimeTick = function(delta) {
-    if (container.state.hitpoints <= 0) {
-      container.destroy();
+
+  lifetimeTick(delta) {
+    if (this.state.hitpoints <= 0) {
+      this.destroy();
     }
   }
-  container.registerHit = function() {
-    container.state.hitpoints -= 1;
+  
+  registerHit() {
+    this.state.hitpoints -= 1;
   }
-  return container;
-};
+}
