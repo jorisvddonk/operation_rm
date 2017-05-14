202534b044df7ced74990d8d6628a65248ea90a2
Joris van de Donk <jorisvddonk@gmail.com>
2017-05-14 16:40:36 +0200

    Refactored ParallaxLayer into an ES6 class

--- client/lib/ParallaxLayer.js
+++ client/lib/ParallaxLayer.js
@@ -1,54 +1,57 @@
 import _ from 'lodash';
 const PIXI = require('pixi.js');
 
-export default function(options) {
-  var container = new PIXI.Container();
-  var texture = PIXI.Texture.fromImage(options.assetLocation);
-  var rendererWidth;
-  var rendererHeight;
-  var depthMultiplier = 1 / options.depth || 1;
-  
-  var update = function() {
-    if (texture.width !== 1 && rendererWidth && rendererHeight) {
+export default class Parallaxlayer extends PIXI.Container {
+  constructor(options) {
+    super();
+    this.texture = PIXI.Texture.fromImage(options.assetLocation);
+    this.rendererWidth;
+    this.rendererHeight;
+    this.depthMultiplier = 1 / options.depth || 1;    
+    this.texture.on('update', () => {
+      this.updateTexture();
+    });
+  }
+
+  updateTexture() {
+    if (this.texture.width !== 1 && this.rendererWidth && this.rendererHeight) {
       // create textures
-      var numHorizontal = Math.max(1, Math.ceil(rendererWidth / texture.width))+2;
-      var numVertical = Math.max(1, Math.ceil(rendererHeight / texture.height))+2;
-      _.each(_.range(0, numHorizontal), function(x){
-        _.each(_.range(0, numVertical), function(y){
-          var sprite = new PIXI.Sprite(texture);
+      var numHorizontal = Math.max(1, Math.ceil(this.rendererWidth / this.texture.width))+2;
+      var numVertical = Math.max(1, Math.ceil(this.rendererHeight / this.texture.height))+2;
+      _.each(_.range(0, numHorizontal), (x) => {
+        _.each(_.range(0, numVertical), (y) => {
+          var sprite = new PIXI.Sprite(this.texture);
           sprite.parallax = {
             x: x,
             y: y
           }
-          sprite.position.x = (x-1) * texture.width;
-          sprite.position.y = (y-1) * texture.height;
-          container.addChild(sprite);
+          sprite.position.x = (x-1) * this.texture.width;
+          sprite.position.y = (y-1) * this.texture.height;
+          this.addChild(sprite);
         });
       });
     }
   }
 
-  texture.on('update', update);
-  
-  container.wire = function(app) {
-    rendererWidth = app.renderer.width;
-    rendererHeight = app.renderer.height;
-    app.ticker.add(function(delta) {
+  wire(app) {
+    this.rendererWidth = app.renderer.width;
+    this.rendererHeight = app.renderer.height;
+    app.ticker.add((delta) => {
       // sync to center of screen
-      container.position.x = app.stage.pivot.x;
-      container.position.y = app.stage.pivot.y;
+      this.position.x = app.stage.pivot.x;
+      this.position.y = app.stage.pivot.y;
       
       // set offset
-      container.pivot.x = (app.stage.pivot.x*depthMultiplier % texture.width) + (container.width*0.5 - texture.width*0.5);
-      container.pivot.y = (app.stage.pivot.y*depthMultiplier % texture.height) + (container.height*0.5 - texture.height*0.5);
-      while (container.pivot.x > texture.width) {
-        container.pivot.x -= texture.width;
+      this.pivot.x = (app.stage.pivot.x*this.depthMultiplier % this.texture.width) + (this.width*0.5 - this.texture.width*0.5);
+      this.pivot.y = (app.stage.pivot.y*this.depthMultiplier % this.texture.height) + (this.height*0.5 - this.texture.height*0.5);
+      while (this.pivot.x > this.texture.width) {
+        this.pivot.x -= this.texture.width;
       }
-      while (container.pivot.y > texture.height) {
-        container.pivot.y -= texture.height;
+      while (this.pivot.y > this.texture.height) {
+        this.pivot.y -= this.texture.height;
       }
     });
-    update();
+    this.updateTexture();
   }
-  return container;
+  
 };
